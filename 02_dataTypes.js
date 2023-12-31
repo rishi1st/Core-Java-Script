c65f56293502b8f72f611da8ed31c29630e617ca
rishi1st <128278988+rishi1st@users.noreply.github.com>
2023-12-31 13:27:13 +0000

    datatypes

---
+++ 02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all Js code as newer version
+// alert(3+3)  //we are using nodejs, not brower
+
+console.log(3 + 3);
+
+let name = "Rishi" // String
+let age = 19 // number
+let IsLogined = true // flase //  = >boolean
+
+// => data type in js
+
+//  number => 2 to power 53
+
+// bigint => use for huge data like trading , etc
+
+// string => prefered "to use  double  quotes "
+
+// boolean => true / false 
+
+// null => stand alone value (representation of empty)
+
+// undefined => mean value is not defined
+
+// symbol => to identify the unique
+
+// ------------- Object -----------
+console.log(typeof "Rishi");
