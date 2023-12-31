0f9653eb1a6d594af2f8bbf1d9a2e1a97bbfec04
rishi1st <128278988+rishi1st@users.noreply.github.com>
2023-12-31 17:41:46 +0000

    Comparision in js

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,16 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+
+
+
+
+
+
+
