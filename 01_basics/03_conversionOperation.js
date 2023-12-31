a924dd29584fd745c7330bbf8039a4a4e309ab11
rishi1st <128278988+rishi1st@users.noreply.github.com>
2023-12-31 14:34:00 +0000

    Conversion of DataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score = "Rishi";
+console.log(typeof score);
+console.log(score);
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "44" => 44
+// "44abc" => NaN (not an number)
+// ture => 1; false => 0;
+
+console.log("------------ Boolean -------------- ");
+let isLogin = 1;
+let booleanisLogin = Boolean(isLogin);
+console.log(booleanisLogin);
+
+// 1 => true; 0 => false
+// "" => false
+// "Rishi" => true;
+
+console.log("----------------String-------------");
+let someNumber = 44
+let stringNumer = String(someNumber);
+console.log(stringNumer);
+console.log(typeof stringNumer);
