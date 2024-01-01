982c76a391d491f2b1f4d57bd507f9d0dcee3f71
rishi1st <128278988+rishi1st@users.noreply.github.com>
2024-01-01 13:29:06 +0000

    comparision in js

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -6,8 +6,21 @@
 
 console.log("2" > 1);
 console.log("02" > 1);
+console.log("null"); // Avoid this type of comperision
+console.log(null > 0);//false
+console.log(null == 0);//false
+console.log(null >= 0);//true
 
+console.log(" undefinde "); // Avoid this type of comperision
+console.log(undefined > 0); // false 
+console.log(undefined == 0); // false
+console.log(undefined >= 0); // false
 
+//                === 
+
+console.log(" == Vs ===");
+console.log("2" == 2);  // check only value
+console.log("2" === 2); // it check data type and the value
 
 
 
