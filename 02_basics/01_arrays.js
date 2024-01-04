c3a3a6ec2d5e0b5c5fe52e68f0955b9b0c7765cb
rishi1st <128278988+rishi1st@users.noreply.github.com>
2024-01-04 14:32:04 +0000

    Array and it's Methods

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,60 @@
+ // array
+
+ const myArr = [0,1,2,3,4,5]
+ const myheros = ["shaktimaan","nagraj", "hanuman"]; 
+ console.table(myArr);
+ console.table(myheros);
+
+ // array as object
+ const myArr2 = new Array(1,2,3,4,5,6);
+ console.log(typeof myArr2);
+ console.log(myArr2);
+
+ // array methods
+
+ // 1. push() used to add at the last position on the array
+ myArr.push(6)
+ console.log(myArr);
+
+ // 2. pop() use to remove last element of the array
+
+myArr.pop()
+
+// 3. usnshift() is used for add element at the first of the element
+myArr.unshift(9);
+
+// 4. shift() used for delete fist element
+myArr.shift();
+// 5. includes() is exist or not
+console.log(myArr.includes(4));
+
+// 6. indexOf() return the index of element
+
+console.log(myArr.indexOf(3));
+
+// 7. join() is used for convert array to string
+
+const newArr = myArr.join();
+
+console.log(myArr)
+console.log(newArr);
+console.log(typeof newArr);
+
+//8. slice , splice
+
+console.log("A" , myArr);
+const myn1 = myArr.slice(1,3);
+console.log(myn1);
+
+
+console.log("B" , myArr);
+const myn2 = myArr.splice(1,3);
+console.log(myn2);
+
+console.log("C" , myArr);
+console.log(myn2);
+
+/*
+⭐⭐⭐
+slice returns a piece of the array but it doesn't affect the original array. splice changes the original array by removing, replacing, or adding values and returns the affected values.
+*/
