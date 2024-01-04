2f1ab7cb0d46d68a4333092c493255935adb840f
rishi1st <128278988+rishi1st@users.noreply.github.com>
2024-01-04 11:44:59 +0000

    Number and Maths

--- 01_basics/06_number_maths.js
+++ 01_basics/06_number_maths.js
@@ -23,4 +23,43 @@ console.log(`Now the number ${hundreds} converted into readable form ${hundreds.
 
 // +++++++++++++++++++++++++++++++++++++ Maths ++++++++++++++++++++++++++++++++++++++++++++++
 
+console.log(Math);
+// abs() 
+console.log(Math.abs(-4));
+//  round is used for get without decimal value and nearest value
+console.log(Math.round(4.6));
+// ceil() is used for maximimum nearest value
+console.log(Math.ceil(4.2));
+// floor() is used for minimum
+console.log(Math.floor(4.8));
+// sqrt()
+ console.log(Math.sqrt(25));
+// pow()
+ console.log(Math.pow(2,6));
+// min()
+console.log(Math.min(1,3,3,63,7));
+// max()
+console.log(Math.max(1,3,3,63,7));
+//  random()
+console.log(`The random number is : ${Math.random()}`);
+/*
+    Without value the random() function generate only numbers between 0 to 1
+
+*/
+console.log(`The number is : ${Math.floor((Math.random()*10 )+ 1)}`);
+/*
+       it define number between 0 to 10
+*/
+
+
+const min = 10
+const max = 20
+
+ console.log(Math.floor(Math.random() * (max-min + 1))+min);
+
+const mn = 1001
+const mx = 9999
+console.log(`Your OTP is :  ${Math.floor(Math.random() * (mx -mn + 1)+mn)}`);
+
+
 // node 01_basics/06_number_maths.js
